1ca8165200f0d56dd545539fefc592ff251f0ff5
KonradWojciak <konradwojciak@gmail.com>
2023-01-30 19:04:41 +0100

    before 4 lesson

--- script.js
+++ script.js
@@ -8,36 +8,40 @@ function myColorChange() {
   element.style.color = "red";
 }
 
-//function mojaFunkcja() {
-//for (let i = 0; i <= 20; i += 5) {
-//   console.log(i);
-//  }
-
-// 1  i iterator odniesienie do zmiennej
-// 2 i warunek końcowy
-// 3 i co ma się zadziać
-
-function myFunction() {
-  const element = document.getElementById("id01");
-  element.innerHTML = "New Heading";
-}
-function myColorChange() {
-  const element = document.getElementById("id01");
-  element.style.color = "red";
-}
 function mojaFunkcja(imie) {
-  const clients = ["Mango", "Poly", "Ajax"];
-  const clientNameToFind = "Patryk";
-  let message;
 
-  for (const client of clients) {
-    if (client === imie) {
-      message = "Klient z takim imieniem jest w bazie danych!";
-      break;
+    const clients = ["Mango", "Poly", "Ajax"];
+    const clientNameToFind = "Patryk";
+    let message; 
+    
+    for (const client of clients) {
+    
+      if (client === imie) {
+        message = "Klient z takim imieniem jest w bazie danych!";
+        break;
+      }
+    
+      message = "Nie znaleźliśmy takiego klienta w bazie danych!";
     }
-
-    message = "Nie znaleźliśmy takiego klienta w bazie danych!";
-  }
-
-  console.log(message);
+    
+    console.log(message);
+    
 }
+
+
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+};
+
+const bookTitle = book.title;
+console.log(bookTitle);// 'The Last Kingdom'
+
+const bookGenres = book.genres;
+console.log(bookGenres);// ['historical prose', 'adventurs']
+
+const bookPrice = book.price;
+console.log(bookPrice);// undefined
